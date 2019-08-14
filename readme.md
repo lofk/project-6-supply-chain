cacce4ef5ca61155a45cbf7d48e13be221d3cba6
Karl Lof <lof.karl@yahoo.fr>
2019-08-14 19:02:41 +0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,33 @@
+## Supply Chain Project
+
+### Requirements
+- NodeJS v8.11.4
+- Truffle v4.1.14 (core: 4.1.14)
+- Solidity v0.4.24 (solc-js)
+
+### Install Application
+```
+npm install
+
+# compile the contracts
+truffle compile
+```
+
+### Run tests
+```
+truffle test
+```
+
+### Development
+```
+npm run dev
+```
+Make sure you have 4 different wallets for each stakeholder, to interact with the smart contract.
+
+### Public Network
+The smart contract for the Supply Chain, can be found on the Rinkeby network.
+
+- Contract transaction hash : https://rinkeby.etherscan.io/tx/0xd4ae7666b3184c1fbba3d2aa5b18efa0bbb411337dcc95402bafd577b92bc8a8
+
+- Contract Address :
+https://rinkeby.etherscan.io/address/0x7192b9383444a582217d363cc7962eb89c8719e8
---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "36ed5a91ce5340d6b5d73620ff59473c";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.24", // A version or constraint - Ex. "^0.5.0"
+      docker: false, // Use a version obtained through docker
+    }
+  }
+};
